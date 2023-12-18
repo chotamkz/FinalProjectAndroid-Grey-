c694c2285e1324e9b239a17006d41f22fba3d6be
shapok <210107108@stu.sdu.edu.kz>
2023-12-18 17:19:01 +0600

    apishka

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp.util
+
+object Constants {
+    const val USER_SETTINGS="userSettings"
+
+    const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "5c9e8b173fe74432bf678860f001c569"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME = "news_db"
+
+}
