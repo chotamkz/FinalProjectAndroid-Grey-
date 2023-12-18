8e86145fca9e8a33c2fb9400c883da947ab8a07d
Daniyar Nurlanov <108024213+chotamkz@users.noreply.github.com>
2023-12-18 17:21:39 +0600

    comm

---
+++ app/src/main/java/com/example/newsapp/presentation/common/NewsButton.kt
@@ -0,0 +1,29 @@
+package com.example.newsapp.presentation.common
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun NewsButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick, colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.Black
+        )
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+        )
+    }
+}
