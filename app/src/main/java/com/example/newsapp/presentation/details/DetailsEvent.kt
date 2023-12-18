bbc10e4564c54c0a8e6e1e95c09e9d41b72fc997
Daniyar Nurlanov <108024213+chotamkz@users.noreply.github.com>
2023-12-18 17:28:17 +0600

    Event

---
+++ app/src/main/java/com/example/newsapp/presentation/details/DetailsEvent.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.presentation.details
+
+import com.example.newsapp.domain.model.Article
+
+sealed class DetailsEvent {
+
+    data class UpsertDeleteArticle(val article: Article): DetailsEvent()
+    object RemoveSideEffect: DetailsEvent()
+}
